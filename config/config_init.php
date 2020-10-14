eea9f91bc5ad7fabd31550c3bf242334185ba1de
Thomas Oudard <thomas.oudard@odigo.com>
2020-10-14 15:46:51 +0200

    Base archi

---
+++ config/config_init.php
@@ -0,0 +1,27 @@
+// Initialisation de la session
+session_start();
+header("Cache-Control: no-cache");
+
+// Chargement Smarty et Defines
+require('defines.inc.php');
+require(_PATH_ . 'web/tools/smarty/Smarty.class.php');
+
+// Chargement du coeur
+$files = scandir(_CORE_);
+foreach ($files as $filename)
+{
+    if (is_file(_CORE_.$filename))
+        require_once(_CORE_.$filename);
+}
+
+// Connexion Database
+try {
+$bdd = new PDO("mysql:host=localhost;dbname=e-commerce", "smarty", "xxrowQyYc1m5a2t3");
+$bdd->query("SET NAMES UTF8");
+} catch (Exception $e) {
+    echo "Problème de connexion à la base de donnée e-commerce...";
+    die();
+}
+
+// Initialisation Smarty
+$smarty = new Smarty();
