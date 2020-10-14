e6f5a1583b6092be09f6e3591796d0d8e2a29ff8
Thomas Oudard <thomas.oudard@odigo.com>
2020-10-14 16:10:10 +0200

    Base archi

--- config/defines.inc.php
+++ config/defines.inc.php
@@ -5,7 +5,7 @@ if (!isset($_SERVER['DOCUMENT_ROOT']))
     die();
 
 // Define de la racine du site
-define('_PATH_', $_SERVER['DOCUMENT_ROOT'].'/');
+define('_PATH_', $_SERVER['DOCUMENT_ROOT'].'/thel');
 
 // Define du dossier Coeur
 define('_CORE_', _PATH_ . 'core/');
