38aa6a431e13574dbecd6a1699ff595f4fc521a4
Raphael Fourel <raph@vps-eb5525fc.vps.ovh.net.novalocal>
2020-10-16 07:04:08 +0000

    chargement coeur config_init.php

--- config/config_init.php
+++ config/config_init.php
@@ -8,12 +8,12 @@ require('defines.inc.php');
 require(_PATH_ . 'web/tools/smarty/Smarty.class.php');
 
 // Chargement du coeur
-// $files = scandir(_CORE_);
-// foreach ($files as $filename)
-// {
-//     if (is_file(_CORE_.$filename))
-//         require_once(_CORE_.$filename);
-// }
+ $files = scandir(_CORE_);
+ foreach ($files as $filename)
+ {
+     if (is_file(_CORE_.$filename))
+         require_once(_CORE_.$filename);
+ }
 
 // Connexion Database
 try {
